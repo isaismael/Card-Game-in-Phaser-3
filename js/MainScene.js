73eca08f52e5ac52c3c066b1149ebf5d2a94dcf7
Ismael Isa <ismael.isa02@gmail.com>
2023-02-21 12:25:50 -0300

    se cargo assets de las cards y font

--- js/MainScene.js
+++ js/MainScene.js
@@ -5,7 +5,21 @@ export default class MainScene extends Phaser.Scene {
     }
 
     preload() {
-        this.loadl.image('card', 'assets/card.png');
+        this.load.image('armor', 'assets/armor.png');
+        this.load.image('card', 'assets/card.png');
+        this.load.image('dead', 'assets/dead.png');
+        this.load.image('deathknight', 'assets/deathknight.png');
+        this.load.image('firedrake', 'assets/firedrake.png');
+        this.load.image('goldendragon', 'assets/goldendragon.png');
+        this.load.image('healingpotion', 'assets/healingpotion.png');
+        this.load.image('kobold', 'assets/kobold.png');
+        this.load.image('ogre', 'assets/ogre.png');
+        this.load.image('paladin', 'assets/paladin.png');
+        this.load.image('playercard', 'assets/playercard.png');
+        this.load.image('restartbutton', 'assets/restartbutton.png');
+        this.load.image('shield', 'assets/shield.png');
+        this.load.image('troll', 'assets/troll.png');
+        this.load.bitmapFont('pressstart', 'assets/pressstart.png', 'assets/pressstart.fnt');
     }
 
 }
